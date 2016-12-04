54cdbea17dfad7ebcf75937b4879bf79303e13f1
NataliaFaundez <natalia.faundez@ug.uchile.cl>
2016-12-04 00:25:26 -0300

    ejemplo de bcast y gather

--- Integral/example_Bcast_Gather.py
+++ Integral/example_Bcast_Gather.py
@@ -8,14 +8,15 @@ root = 0
 
 buf = 0
 buf_list = None
-if rank == root:
-    buf = 777
+if rank == root: #procesador 0, madre
+    buf = 777  #bur se modifica en procesador 0 
 
-print "[%d]: Before Bcast, buf is %d\n"%(rank, buf)
-buf = comm.bcast(buf, root=root)
-print "[%d]: Before Bcast, buf is %d\n"%(rank, buf)
-buf = buf + rank
-buf_list = comm.gather(buf, root=root)
+print "[%d]: Before Bcast, buf is %d\n"%(rank, buf) # se imprime en todos los procesadores 
+#porque esta afuera y solo la madre es 777 el resto es 0
+buf = comm.bcast(buf, root=root)  #en todos los procesadores (1 al 3)el buf es modificado a 777 por el buf que esta en madre 
+print "[%d]: Before Bcast, buf is %d\n"%(rank, buf) # se imprime en todos los procesadores
+buf = buf + rank # a cada buf se le agrega la posicion del procesador
+buf_list = comm.gather(buf, root=root) #agarra los buf de los procesadores y los devuelve a la lista buf en el procesador madre
 
-if rank ==root:
-    print buf_list
+if rank ==root: #estando en posicion madre
+    print buf_list #imprime lista buf
